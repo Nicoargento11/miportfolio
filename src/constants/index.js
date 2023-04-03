898611b7f5041f4f3ef95edf964d79b2d3cea5bb
Nicoargento11 <valdes.nicoedu@gmail.com>
2023-04-03 15:24:57 -0300

    portfolio structure done

--- src/constants/index.js
+++ src/constants/index.js
@@ -198,9 +198,9 @@ import {
   
   const projects = [
     {
-      name: "Car Rent",
+      name: "mini-world",
       description:
-        "Web-based platform that allows users to search, book, and manage car rentals from various providers, providing a convenient and efficient solution for transportation needs.",
+        "aplicacion web basada en ordenamiento, filtrado y busquedas de paises de todo el mundo traidos a traves de una API y guardados en una base de datos relacional",
       tags: [
         {
           name: "react",
@@ -219,9 +219,9 @@ import {
       source_code_link: "https://github.com/",
     },
     {
-      name: "Job IT",
+      name: "Flex-agenda",
       description:
-        "Web application that enables users to search for job openings, view estimated salary ranges for positions, and locate available jobs based on their current location.",
+        "aplicacion web basada en una agenda virtual, dando asi al profesional la capacidad de llevar un mejor control de sus turnos y clientes.",
       tags: [
         {
           name: "react",
@@ -239,27 +239,27 @@ import {
       image: jobit,
       source_code_link: "https://github.com/",
     },
-    {
-      name: "Trip Guide",
-      description:
-        "A comprehensive travel booking platform that allows users to book flights, hotels, and rental cars, and offers curated recommendations for popular destinations.",
-      tags: [
-        {
-          name: "nextjs",
-          color: "blue-text-gradient",
-        },
-        {
-          name: "supabase",
-          color: "green-text-gradient",
-        },
-        {
-          name: "css",
-          color: "pink-text-gradient",
-        },
-      ],
-      image: tripguide,
-      source_code_link: "https://github.com/",
-    },
+    // {
+    //   name: "Coming soon",
+    //   description:
+    //     "coming soon.",
+    //   tags: [
+    //     {
+    //       name: "nextjs",
+    //       color: "blue-text-gradient",
+    //     },
+    //     {
+    //       name: "supabase",
+    //       color: "green-text-gradient",
+    //     },
+    //     {
+    //       name: "css",
+    //       color: "pink-text-gradient",
+    //     },
+    //   ],
+    //   image: tripguide,
+    //   source_code_link: "https://github.com/",
+    // },
   ];
   
   export { services, technologies, experiences, testimonials, projects };
