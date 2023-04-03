b473b5862ed163d03f24af6dfc729c80ed49c8e1
Nicoargento11 <valdes.nicoedu@gmail.com>
2023-04-03 12:04:53 -0300

    navbar, hero, tech,works, experiencie done

---
+++ src/components/About.jsx
@@ -0,0 +1,61 @@
+import React from "react";
+import { Tilt } from "react-tilt";
+import { motion } from "framer-motion";
+
+import { styles } from "../styles";
+import { services } from "../constants/index";
+import { fadeIn, textVariant } from "../utils/motion";
+import { SectionWrapper } from "../hoc";
+const ServiceCard = ({ index, title, icon }) => {
+  return (
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full green-pink-gradient p-[1px] rounded-[20px]  shadow-card"
+      >
+        <div
+          options={{ max: 45, scale: 1, speed: 450 }}
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          <img src={icon} alt={icon} className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
+        </div>
+      </motion.div>
+    </Tilt>
+  );
+};
+
+const About = () => {
+  return (
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction </p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className="mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]"
+      >
+        ¡Hola! Me llamo Nicolas y soy desarrollador de aplicaciones web. Me
+        apasiona la tecnologia y me encanta utilizar mis habilidades para crear
+        aplicaciones únicas. He aprendido a ser perseverante en lo que hago y
+        siempre he buscado la forma de disfrutarlo, ya que es la mejor manera de
+        que perdure con el tiempo. Actualmente estoy estudiando licenciatura en
+        sistemas de información y he trabajado en varios proyectos tanto
+        individual como grupal. Mis habilidades principales incluyen React,
+        Node,js, Tailwindcss, Postgress y Express. Si tienes alguna pregunta o
+        quieres saber más sobre mi trabajo, no dudes en ponerte en contacto
+        conmigo. ¡Gracias por leer!
+      </motion.p>
+      <div className="mt-20 flex flex-wrap gap-10">
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
